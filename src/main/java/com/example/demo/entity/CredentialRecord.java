909060bd597a5f6b612392bc024303dc44ea1b29
bala-amypo <bala@amypo.in>
2025-12-20 05:33:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/CredentialRecord.java
+++ src/main/java/com/example/demo/entity/CredentialRecord.java
@@ -1,12 +1,10 @@
 package com.example.demo.entity;
+
 import jakarta.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
+import java.util.HashSet;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 @Table(
