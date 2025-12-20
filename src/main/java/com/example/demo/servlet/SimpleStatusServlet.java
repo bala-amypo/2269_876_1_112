ed542c6c397b360384761381294b92e52ca65326
bala-amypo <bala@amypo.in>
2025-12-20 14:24:32 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 public class SimpleStatusServlet extends HttpServlet {
     
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+     void doGet(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
         resp.setContentType("text/plain");
         PrintWriter writer = resp.getWriter();
