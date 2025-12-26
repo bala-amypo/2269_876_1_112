27406bb47034c626e92ad9629571d08801d9a792
bala-amypo <bala@amypo.in>
2025-12-26 07:45:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/VerificationRuleRepository.java
+++ src/main/java/com/example/demo/repository/VerificationRuleRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface VerificationRuleRepository
         extends JpaRepository<VerificationRule, Long> {
+                List<VerificationRule> findByActiveTrue();
 }
